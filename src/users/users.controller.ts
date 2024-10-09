deca798cc1f0aa2eeb084ee5322d3a064cb070af
Lucas Oliveira <lucas7justino@gmail.com>
2024-10-09 01:29:39 -0300

    feat: setting up signup route and user dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dtos/create-user.dto";
 
 @Controller('users')
-export class UsersController {};
+export class UsersController {
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto) {}
+};
