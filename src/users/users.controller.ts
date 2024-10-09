ab497d7277437253db7f91e91ea8c7153428cf61
Lucas Oliveira <lucas7justino@gmail.com>
2024-10-09 01:41:18 -0300

    refactor: renaming user controller to auth controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {}
+    createUser(@Body() body: CreateUserDto) {
+        console.log(body);
+    }
 };
