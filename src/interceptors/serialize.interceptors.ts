2c1cb9a8b9e623606a1425c2ff1b656609b3f0e7
Lucas Oliveira <lucas7justino@gmail.com>
2024-10-11 22:03:24 -0300

    feat: setted up custom interceptor to not show password as response

--- src/interceptors/serialize.interceptors.ts
+++ src/interceptors/serialize.interceptors.ts
@@ -2,15 +2,19 @@ import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from 
 import { Observable } from "rxjs"
 import { map } from 'rxjs/operators'
 import { plainToClass } from "class-transformer"
+import { UserDto } from "src/users/dtos/user.dto"
 
-export class SerializeInterceptor implements NestInterceptor{
+export class SerializeInterceptor implements NestInterceptor {
+
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-        console.log("I'm running before the handler", context);
 
         return handler.handle().pipe(
             map((data: any) => {
-                console.log('Im running before the response is sent out', data);
+                return plainToClass(UserDto, data, {
+                    excludeExtraneousValues: true,
+                });
             })
-        )
+        );
+
     }
 }
