1e412b65f4a2c09f48097119cd354a43de0b757a
lucasjstn <lucas7justino@gmail.com>
2024-10-13 01:41:45 -0300

    feat: setting up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('Football Manager API')
+    .setDescription('Minha api do meu projeto pessoal que não irei terminar')
+    .setVersion('1.0')
+    .addTag('Naosei')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3001);
 }
 bootstrap();
