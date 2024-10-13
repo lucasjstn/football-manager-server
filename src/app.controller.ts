f30400461d690ec6b31491f7f2ff6a6550458650
lucasjstn <lucas7justino@gmail.com>
2024-10-13 01:42:21 -0300

    feat: add up  user authentication with hashing method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
+  @ApiOperation({ summary: 'exemplo' })
+  @ApiResponse({ status: 200, description: 'Rota funcionando' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -18,41 +18,78 @@ export class AuthService {
     constructor(private usersService: UsersService) { }
 
     async signup(email: string, password: string, username: string) {
-        // See if email is in use
         const [users] = await this.usersService.find(username);
 
-        console.log(users.email, users.username);
 
-        if (users.email && users.username) {
+        if (users?.email && users?.username) {
             throw new BadRequestException('email in use');
         }
 
-        const hashedPassword = await this.hashPassword(password);
+        const salt = randomBytes(8).toString('hex');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
 
-        const user = await this.usersService.create(email, username, hashedPassword);
+        const result = salt + "." + hash.toString('hex');
+
+        const user = this.usersService.create(email, username, result);
 
         return user;
+        // See if email is in use
+        // const [users] = await this.usersService.find(username);
+
+
+        // if (users?.email && users?.username) {
+        //     throw new BadRequestException('email in use');
+        // }
+
+        // const hashedPassword = await this.hashPassword(password);
+
+        // const user = await this.usersService.create(email, username, hashedPassword);
+
+        // return user;
     }
 
     async signin(username: string, password: string) {
+
         const [user] = await this.usersService.find(username);
 
-        console.log(user);
-
-        if (!user) {
+        if(!user){
             throw new NotFoundException('user not found');
         }
 
+        console.log(user);
+        
+        const [salt, storedHash] = user.password.split('.');
 
-        const isPasswordValid = await this.comparePasswords(password, user.password);
-
-        console.log(isPasswordValid);
-
-        if (!isPasswordValid) {
-            throw new BadRequestException('Invalid password');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        console.log(storedHash);
+        console.log(hash.toString('hex'));
+        if(storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('bad password');
         }
 
-        return user; // Retorna o usuário se as credenciais forem válidass
+       
+        
+
+        return user;
+
+        // const [user] = await this.usersService.find(username);
+
+        // console.log(user);
+
+        // if (!user) {
+        //     throw new NotFoundException('user not found');
+        // }
+
+
+        // const isPasswordValid = await this.comparePasswords(password, user.password);
+
+        // console.log(isPasswordValid);
+
+        // if (!isPasswordValid) {
+        //     throw new BadRequestException('Invalid password');
+        // }
+
+        // return user; // Retorna o usuário se as credenciais forem válidass
     }
 
     private async hashPassword(password: string): Promise<string> {
