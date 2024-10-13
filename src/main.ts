69e82c52b06327a6af7dafc56f55498ca62a188f
lucasjstn <lucas7justino@gmail.com>
2024-10-13 09:52:33 -0300

    chore: first cookies session settings

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: [process.env.COOKIE_KEY],
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
