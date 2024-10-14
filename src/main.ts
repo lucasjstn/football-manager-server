a231d026d2c879dfda1128d16e3f9043977bb931
lucasjstn <lucas7justino@gmail.com>
2024-10-13 21:00:14 -0300

    feat: add session to user and whoami route

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieSession({
-    keys: [process.env.COOKIE_KEY],
+    keys: ['secretsecret'],
   }))
   app.useGlobalPipes(
     new ValidationPipe({
